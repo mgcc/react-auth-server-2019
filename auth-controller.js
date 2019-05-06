e2997c323ce822211b6ef59104f0176ca13d16cb
Monina Carandang <reactor04@gmail.com>
2019-05-06 20:49:10 +0800

    Made clean version

--- auth-controller.js
+++ auth-controller.js
@@ -12,8 +12,7 @@ exports.signup = (req, res) => {
   })
 
   newUser.save((err) => {
-    if (err) return res.send({ success: false })
-    else return res.send({ success: true })
+    // ---
   });
 }
 
@@ -26,14 +25,14 @@ exports.login = (req, res) => {
     // Check if email exists
     if (err || !user) {
       // Scenario 1: FAIL - User doesn't exist
-      return res.send({ success: false })
+      // ---
     }
 
     // Check if password matches
     user.comparePassword(password, (err, isMatch) => {
       if (err || !isMatch) {
         // Scenario 2: FAIL - Wrong password
-        return res.send({ success: false })
+        // ---
       }
 
       // Scenario 3: SUCCESS - time to create token
@@ -44,7 +43,7 @@ exports.login = (req, res) => {
       const token = jwt.sign(tokenPayload, 'THIS_IS_A_SECRET')
 
       // Return token to front end
-      return res.send({ success: true, token, username: user.name })
+      // ---
     })
   })
 
@@ -53,14 +52,14 @@ exports.login = (req, res) => {
 exports.checkIfLoggedIn = (req, res) => {
   if (!req.cookies || !req.cookies.authToken) {
     // Scenario 1: FAIL - No cookies / No auth token sent
-    return res.send({ loggedIn: false })
+    // ---
   }
 
   // Validate token if found
   return jwt.verify(req.cookies.authToken, 'THIS_IS_A_SECRET', (err, decoded) => {
     if (err) {
       // Scenario 2: FAIL - Error validating token
-      return res.send({ loggedIn: false })
+      // ---
     }
 
     const userId = decoded._id
@@ -69,11 +68,11 @@ exports.checkIfLoggedIn = (req, res) => {
     return User.findById(userId, (userErr, user) => {
       if (userErr || !user) {
         // Scenario 3: FAIL - Failed to find user based on id inside token
-        return res.send({ loggedIn: false })
+        // ---
       }
 
       // Scenario 4: SUCCESS - Token and user data are valid
-      return res.send({ loggedIn: true })
+      // ---
     })
   })
 
